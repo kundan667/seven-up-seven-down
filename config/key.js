2803f71df7f8340de68bba534d876dcda387722d
Kundan choudhary <zrkundan@gmail.com>
2021-05-02 14:57:27 +0530

    db config

--- config/key.js
+++ config/key.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: "mongodb://127.0.0.1:27017/sevengame",
+    mongoURIprod: "mongodb+srv://kundan:kundan667@heroku.frcdd.mongodb.net/seven-up-seven-down?retryWrites=true&w=majority",
     sercretKey: "secret"
 }
