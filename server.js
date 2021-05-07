6ffd06f5d84805a53741abfad31dd50147332d60
Kundan choudhary <zrkundan@gmail.com>
2021-05-07 17:26:16 +0530

    commit

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //app.use(passport.initialize());
 
-// API Routes
-app.use("/api", index);
-
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/dist'));
 }
+
+// API Routes
+app.use("/api", index);
 
 app.get('*',(req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
